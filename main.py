b4f4b73d6efa1298eb3c77db8a07f1cff73e7a0e
Badara Diallo <badaradiallo@Host-001.lan>
2025-06-26 18:31:34 +0200

    Data Ingestion

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+from src.cnnClassifier.pipeline.data_ingestion_pipeline import (
+    DataIngestionTrainingPippeline,
+)
 from src.cnnClassifier import logger
 
-logger.info("Welcome to my custom log")
+
+STAGE_NAME = "Data Ingestion Stage"
+
+if __name__ == "__main__":
+    try:
+        data_ingestion = DataIngestionTrainingPippeline()
+        data_ingestion.initiate_data_ingestion()
+        logger.info(f"Stage {STAGE_NAME} completed")
+    except Exception as e:
+        raise logger.info(e)
